178ea863c04f004a267f10ed76d9452451e9f761
Ajay Krishnan <50063680+ajaynomics@users.noreply.github.com>
2024-01-24 06:48:55 -0500

    refactor tests with setup hooks

--- test/cloudflare/ai/results/text_generation_test.rb
+++ test/cloudflare/ai/results/text_generation_test.rb
@@ -1,17 +1,19 @@
 require "test_helper"
 
 class Cloudflare::AI::Results::TextGenerationTest < Minitest::Test
-  def test_successful_result
-    result = Cloudflare::AI::Results::TextGeneration.new(successful_response_json)
-    assert result.success?
-    refute result.failure?
+  def setup
+    @result = Cloudflare::AI::Results::TextGeneration.new(successful_response_json)
+  end
 
-    assert_equal successful_response_json["result"]["response"], result.response
+  def test_successful_result
+    assert @result.success?
+    refute @result.failure?
+
+    assert_equal successful_response_json["result"]["response"], @result.response
   end
 
   def test_to_json
-    result = Cloudflare::AI::Results::TextGeneration.new(successful_response_json)
-    assert_equal successful_response_json.to_json, result.to_json
+    assert_equal successful_response_json.to_json, @result.to_json
   end
 
   private
